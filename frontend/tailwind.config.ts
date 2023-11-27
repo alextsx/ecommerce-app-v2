9411e0eee10e470daf8816a3cd94a0119c9f0218
alextsx <c0ltwinters@protonmail.com>
2023-11-27 20:14:38 +0100

    chore: update shadcn configuration

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -69,9 +69,6 @@ module.exports = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out'
-      },
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif']
       }
     }
   },
