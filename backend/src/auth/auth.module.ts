12a5e960336cab2d9d1b227734552a673a50c339
alextsx <c0ltwinters@protonmail.com>
2023-11-27 01:17:48 +0100

    fix: add missing service to providers

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './services/auth.service';
 import { PasswordService } from './services/password.service';
+import { TokenService } from './services/token.service';
 import { AtStrategy, RtStrategy } from './strategies';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, AtStrategy, RtStrategy, PasswordService]
+  providers: [AuthService, AtStrategy, RtStrategy, PasswordService, TokenService]
 })
 export class AuthModule {}
