2080f21620ad16a43af211c3650089b6ed06a76f
alextsx <c0ltwinters@protonmail.com>
2023-11-28 20:50:03 +0100

    feat: change footer bg styling

--- frontend/components/footer/Footer.tsx
+++ frontend/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="py-6  flex flex-col items-center justify-between gap-4 h-20 w-full bg-primary">
+    <footer className="py-6  flex flex-col items-center justify-between gap-4 h-20 w-full bg-primary/80">
       <p className="text-center text-sm leading-loose text-primary-foreground">
         Built by John Doe with <a href="https://nextjs.org">Next.js</a> and{' '}
         <a href="https://tailwindcss.com">Tailwind CSS</a>
