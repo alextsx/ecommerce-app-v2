fb74e44188a28c9df49717de06982a6a240e9f6b
alextsx <c0ltwinters@protonmail.com>
2023-11-28 18:01:31 +0100

    feat: change footer design

--- frontend/components/footer/Footer.tsx
+++ frontend/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <footer className="py-6 container flex flex-col items-center justify-between gap-4 h-20">
-      <p className="text-center text-sm leading-loose text-muted-foreground">
+    <footer className="py-6  flex flex-col items-center justify-between gap-4 h-20 w-full bg-primary">
+      <p className="text-center text-sm leading-loose text-primary-foreground">
         Built by John Doe with <a href="https://nextjs.org">Next.js</a> and{' '}
         <a href="https://tailwindcss.com">Tailwind CSS</a>
       </p>
