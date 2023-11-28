ac365439f00a07b92f6aa706d9dbc4d1c0dcfc03
alextsx <c0ltwinters@protonmail.com>
2023-11-28 20:52:36 +0100

    feat: create SubmitBtn component

---
+++ frontend/components/SubmitBtn.tsx
@@ -0,0 +1,20 @@
+import { ComponentPropsWithoutRef, memo } from 'react';
+import { Button } from '@/components/ui/button';
+import { Spinner } from './Spinner';
+
+type SubmitBtnProps = {
+  isLoading: boolean;
+  isBtnDisabled: boolean;
+  label: string;
+} & ComponentPropsWithoutRef<'button'>;
+
+export const SubmitBtn = memo(({ isBtnDisabled, isLoading, label, ...rest }: SubmitBtnProps) => {
+  return (
+    <Button type="submit" disabled={isBtnDisabled} {...rest}>
+      {isLoading && <Spinner className="mr-2 h-4 w-4 animate-spin" />}
+      {label}
+    </Button>
+  );
+});
+
+SubmitBtn.displayName = 'SubmitBtn';
