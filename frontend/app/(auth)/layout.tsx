6145eef7278943cdca0203238c51a16174b3a861
alextsx <c0ltwinters@protonmail.com>
2023-11-30 22:05:49 +0100

    fix: auth layout thats consistent for both login and registration

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 
 const AuthLayout = ({ children }: PropsWithChildren) => (
-  <div className="sm:w-full  md:w-7/12 py-20 max-h-[850px] overflow-y-scroll flex justify-center items-center bg-background rounded-lg p-1 border">
+  <div className="md:w-7/12 w-full flex max-h-[600px] overflow-y-scroll justify-center items-center bg-background rounded-md p-1 border">
     {children}
   </div>
 );
--- frontend/app/(auth)/registration/page.tsx
+++ frontend/app/(auth)/registration/page.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
-import { RegistrationForm } from '@/components/forms/RegistrationForm';
+import { RegistrationForm } from '@/components/form/RegistrationForm';
 
 const RegistrationPage = () => {
   return (
-    <div className="z-20 flex flex-col justify-center items-between">
+    <div className="z-20 py-14 w-full flex flex-col justify-center items-center">
       <div className=" flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center mb-3">
           <h1 className="text-3xl font-semibold tracking-tight">Create an account</h1>
