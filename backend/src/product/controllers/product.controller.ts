e7b210edf78fbbea2cdd22ef396795ee3b8740ed
alextsx <c0ltwinters@protonmail.com>
2023-12-03 01:52:12 +0100

    feat: create custom transformData interceptor

--- backend/src/product/controllers/product.controller.ts
+++ backend/src/product/controllers/product.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, UseInterceptors } from '@nestjs/common';
+import { Product } from '@prisma/client';
 import { Public } from 'src/common/decorators';
+import { TransformDataInterceptor } from 'src/common/interceptors/transformData.interceptor';
 import { ProductDto } from '../dtos/product.dto';
 import { ProductService } from '../services/product.service';
 
@@ -9,7 +11,8 @@ export class ProductController {
 
   @Public()
   @Get(':slug')
-  async getProduct(@Param('slug') slug: string): Promise<ProductDto | null> {
+  @UseInterceptors(new TransformDataInterceptor(ProductDto))
+  async getProduct(@Param('slug') slug: string): Promise<Product> {
     const product = await this.productService.getProductWithRelatedInformation(slug);
     if (!product) {
       throw new NotFoundException(`Product with slug ${slug} not found`);
--- backend/src/product/services/product.service.ts
+++ backend/src/product/services/product.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { plainToClass } from 'class-transformer';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { ProductDto } from '../dtos/product.dto';
 
 @Injectable()
 export class ProductService {
   constructor(private prismaService: PrismaService) {}
   async getProductWithRelatedInformation(slug: string) {
-    const record = await this.prismaService.product.findUnique({
+    return this.prismaService.product.findUnique({
       where: {
         slug
       },
@@ -24,8 +22,6 @@ export class ProductService {
         }
       }
     });
-
-    return plainToClass(ProductDto, record);
   }
 
   async getRelatedProducts(slug: string) {
