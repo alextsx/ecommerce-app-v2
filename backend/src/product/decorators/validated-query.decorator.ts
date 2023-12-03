351b68e5e6595986732c741b20d8b6d51903d706
alextsx <c0ltwinters@protonmail.com>
2023-12-03 20:11:35 +0100

    feat: create ValidatedQuery custom decorator

---
+++ backend/src/product/decorators/validated-query.decorator.ts
@@ -0,0 +1,9 @@
+import { Query, ValidationPipe } from '@nestjs/common';
+
+export const ValidatedQuery = () =>
+  Query(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true
+    })
+  );
