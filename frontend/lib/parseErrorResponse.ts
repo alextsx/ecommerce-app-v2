ff07e08e9af0875e02e287f6e16ec694e8795441
alextsx <c0ltwinters@protonmail.com>
2023-12-07 00:59:33 +0100

    feat: change the way parseError parses error messages

--- frontend/lib/parseErrorResponse.ts
+++ frontend/lib/parseErrorResponse.ts
@@ -1,8 +1,12 @@
-export const parseErrorResponse = (error: unknown): string | string[] => {
+export const parseErrorResponse = (error: unknown): string => {
   const { data } = error as ErrorResponseType;
 
   if (!data || !data.message) {
     return 'Something went wrong';
+  }
+
+  if (Array.isArray(data.message)) {
+    return data.message[0];
   }
 
   return data.message;
