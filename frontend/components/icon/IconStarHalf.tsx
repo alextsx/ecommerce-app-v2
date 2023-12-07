6e37518d854fc51edc5a3928e970a2128841d94d
alextsx <c0ltwinters@protonmail.com>
2023-12-08 00:40:29 +0100

    feat: create reusable ProductRating component

---
+++ frontend/components/icon/IconStarHalf.tsx
@@ -0,0 +1,29 @@
+import { HTMLAttributes } from 'react';
+
+export const IconStarHalf = ({ className }: HTMLAttributes<SVGElement>) => {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <defs>
+        <linearGradient id="half-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+          <stop offset="50%" stopColor="currentColor" />
+          <stop offset="50%" stopColor="transparent" stopOpacity="1" />
+        </linearGradient>
+      </defs>
+      <polygon
+        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
+        fill="url(#half-gradient)"
+      />
+    </svg>
+  );
+};
---
+++ frontend/components/page-specific/product/ProductRating.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { IconStar } from '@/components/icon/IconStar';
+import { IconStarHalf } from '@/components/icon/IconStarHalf';
+
+export const ProductRating = ({ rating }: { rating: number }) => {
+  const fullStars = Math.floor(rating);
+  const halfStar = rating % 1 !== 0;
+  const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
+  return (
+    <>
+      {[...Array(fullStars)].map((_, i) => (
+        <IconStar key={i} className="w-5 h-5 fill-primary" />
+      ))}
+      {halfStar && <IconStarHalf className="w-5 h-5 fill-primary" />}
+      {[...Array(emptyStars)].map((_, i) => (
+        <IconStar
+          key={i + fullStars + (halfStar ? 1 : 0)}
+          className="w-5 h-5 fill-muted stroke-muted-foreground"
+        />
+      ))}
+    </>
+  );
+};
