2ba035fd96de124c4aeb225080d7e6779cb603e9
alextsx <c0ltwinters@protonmail.com>
2023-12-07 01:00:29 +0100

    fix: change invalid conditional rendering condition for guest user/logged in user icon

--- frontend/components/layout/header/user/UserDropdownBtn.tsx
+++ frontend/components/layout/header/user/UserDropdownBtn.tsx
@@ -5,15 +5,17 @@ import { useSelector } from 'react-redux';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { selectAccessToken } from '@/redux/auth/auth.slice';
 import { selectNameInitials } from '@/redux/user-details/user-details.slice';
 
 export const UserDropdownBtn = () => {
   const name_initials = useSelector(selectNameInitials);
+  const accessToken = useSelector(selectAccessToken);
 
   return (
     <DropdownMenuTrigger asChild>
       <Button variant="ghost" className="relative h-10 w-10 p-1 rounded-full">
-        {!name_initials ? (
+        {!accessToken ? (
           <FaUserCircle className="h-10 w-10 text-muted-foreground" />
         ) : (
           <Avatar className="h-10 w-10">
