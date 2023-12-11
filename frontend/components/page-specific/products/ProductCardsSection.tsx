c9d6243b3b54676ce6664df1668030dd95009000
alextsx <c0ltwinters@protonmail.com>
2023-12-11 02:50:01 +0100

    feat: change RTK query hook to use parsed searchparams for requests

--- frontend/components/page-specific/products/ProductCardsSection.tsx
+++ frontend/components/page-specific/products/ProductCardsSection.tsx
@@ -1,14 +1,18 @@
 'use client';
 
+import { useSearchParams } from 'next/navigation';
 import { ProductCard } from '@/components/card/ProductCard';
+import { parseProductsSearchParams } from '@/lib/parseProductsSearchParams';
 import { useGetProductsQuery } from '@/redux/products/products.api.slice';
 
 export const ProductCardsSection = () => {
-  const { data: products, isLoading, isFetching } = useGetProductsQuery();
+  const searchParams = useSearchParams();
+  const parsedSearchParams = parseProductsSearchParams(searchParams);
+  const { data: products, isLoading, isFetching } = useGetProductsQuery(parsedSearchParams);
 
   if (isLoading || isFetching) return <div>Loading...</div>;
 
-  if (!products) return <div>No products found</div>;
+  if (!products || !products.length) return <div>No products found</div>;
 
   return (
     <div className="grid grid-cols-3 gap-y-10 gap-x-8 justify-center">
