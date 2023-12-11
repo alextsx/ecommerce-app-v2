6f669012d23d300eb47681f8c163e797705b7e69
alextsx <c0ltwinters@protonmail.com>
2023-12-11 02:48:48 +0100

    fix: initial pricefilter state now defaults to pricerange correctly without mutating url

--- frontend/components/page-specific/products/filters/PriceFilter.tsx
+++ frontend/components/page-specific/products/filters/PriceFilter.tsx
@@ -1,34 +1,44 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { Slider } from '@/components/ui/slider';
 import { useUpdateURL } from '@/hooks/useUpdateURL';
 import { useGetPriceRangeQuery } from '@/redux/products/products.api.slice';
 
 export const PriceFilter = () => {
-  const searchParams = useSearchParams();
-  const { updateURL } = useUpdateURL();
-  const initialMinPrice = searchParams.get('minPrice') || 0;
-  const initialMaxPrice = searchParams.get('maxPrice') || 100;
-
-  const [minPrice, setMinPrice] = useState<number>(Number(initialMinPrice));
-  const [maxPrice, setMaxPrice] = useState<number>(Number(initialMaxPrice));
+  const [minPrice, setMinPrice] = useState<number>(0);
+  const [maxPrice, setMaxPrice] = useState<number>(0);
+  const [userMinPrice, setUserMinPrice] = useState<number | null>(null);
+  const [userMaxPrice, setUserMaxPrice] = useState<number | null>(null);
 
   const { data: priceRange, isLoading, isFetching } = useGetPriceRangeQuery();
+  const { updateURL } = useUpdateURL();
 
   const handleValueChange = ([min, max]: [number, number]) => {
     setMinPrice(min);
     setMaxPrice(max);
+    setUserMinPrice(min);
+    setUserMaxPrice(max);
   };
 
   useEffect(() => {
-    updateURL('minPrice', String(minPrice));
-  }, [minPrice, updateURL]);
+    if (priceRange) {
+      setMinPrice(priceRange.min);
+      setMaxPrice(priceRange.max);
+    }
+  }, [priceRange]);
 
   useEffect(() => {
-    updateURL('maxPrice', String(maxPrice));
-  }, [maxPrice, updateURL]);
+    if (userMinPrice !== null) {
+      updateURL('minPrice', String(userMinPrice));
+    }
+  }, [userMinPrice, updateURL]);
+
+  useEffect(() => {
+    if (userMaxPrice !== null) {
+      updateURL('maxPrice', String(userMaxPrice));
+    }
+  }, [userMaxPrice, updateURL]);
 
   return (
     <div>
