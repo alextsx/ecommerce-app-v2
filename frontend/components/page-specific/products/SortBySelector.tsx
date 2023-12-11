d344bec472192ef1ace821a01834e052440190e9
alextsx <c0ltwinters@protonmail.com>
2023-12-11 02:16:28 +0100

    feat: implement sorting functionality on frontend

--- frontend/components/page-specific/products/SortBySelector.tsx
+++ frontend/components/page-specific/products/SortBySelector.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { HTMLAttributes } from 'react';
+import { useSearchParams } from 'next/navigation';
 import {
   Select,
   SelectContent,
@@ -6,19 +9,32 @@ import {
   SelectTrigger,
   SelectValue
 } from '@/components/ui/select';
+import { useUpdateURL } from '@/hooks/useUpdateURL';
 import { cn } from '@/lib/shadcn-utils';
 
 export const ProductsSortBySelector = ({ className }: HTMLAttributes<typeof Select>) => {
+  const searchParams = useSearchParams();
+  const selectedSort = searchParams.get('sort') || '';
+  const { updateURL } = useUpdateURL();
+
+  const onSortSelect = (sort: string) => {
+    if (sort === selectedSort) {
+      updateURL('sort', null);
+    } else {
+      updateURL('sort', sort);
+    }
+  };
+
   return (
-    <Select defaultValue="1">
+    <Select onValueChange={onSortSelect} value={selectedSort}>
       <SelectTrigger className={cn(className, 'w-60')}>
         <SelectValue placeholder="Select Sorting" />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem value="1">Price: Low to High</SelectItem>
-        <SelectItem value="2">Price: High to Low</SelectItem>
-        <SelectItem value="3">Reviews: High to Low</SelectItem>
-        <SelectItem value="4">Reviews: Low to High</SelectItem>
+        <SelectItem value="price_asc">Price: Low to High</SelectItem>
+        <SelectItem value="price_desc">Price: High to Low</SelectItem>
+        <SelectItem value="rating_desc">Reviews: High to Low</SelectItem>
+        <SelectItem value="rating_asc"> Reviews: Low to High</SelectItem>
       </SelectContent>
     </Select>
   );
