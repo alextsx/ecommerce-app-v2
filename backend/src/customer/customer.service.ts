e331a8edbea67697997886c567610771c47ed053
alextsx <c0ltwinters@protonmail.com>
2023-12-26 20:16:31 +0100

    feat: implement create customer service

--- backend/src/customer/customer.service.ts
+++ backend/src/customer/customer.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCustomerDto } from './dtos/create-customer.dto';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  public async createCustomer(data: CreateCustomerDto) {
+    return this.prismaService.customer.create({
+      data
+    });
+  }
+}
